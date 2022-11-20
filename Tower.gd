474537b20b7fc52971e47513553eec773d5cb05a
Benjamin Tucker <benjamin.tucker@hey.com>
2022-11-19 21:28:03 -0800

    show tower progress

--- Tower.gd
+++ Tower.gd
@@ -1,9 +1,14 @@
 class_name Tower
 extends Node2D
 
+export var rocks_needed := 10
 var rocks := 0
 
 func _interact(bird: Bird) -> void:
 	if bird.has_rock:
 		bird.remove_rock()
 		rocks += 1
+
+func _process(_delta):
+	var pb := $ProgressBar as ProgressBar
+	pb.value = clamp(rocks / float(rocks_needed), 0, 1)
