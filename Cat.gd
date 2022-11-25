1ec8ef055ace0ca2e127dacbb588a4398c6b1b5b
Benjamin Tucker <benjamin.tucker@hey.com>
2022-11-24 21:07:28 -0800

    fix cat usage of %

--- Cat.gd
+++ Cat.gd
@@ -5,6 +5,7 @@ signal died()
 var async_action
 var target: Node2D
 onready var mover := $PathMover as PathMover
+onready var birdnav := $"/root/Main".get_node(@"%BirdNav") as BirdNav
 
 func _process(_delta):
 	if target && !is_instance_valid(target):
@@ -29,7 +30,7 @@ func find_and_kill():
 	var bird := yield(find_nearest_bird(), "completed") as Node2D
 	yield(wait, "timeout")
 	if !is_instance_valid(bird): return
-	mover.current_path = ($"%BirdNav" as BirdNav).find_path(global_position, bird.global_position)
+	mover.current_path = birdnav.find_path(global_position, bird.global_position)
 	yield(mover, "destination_reached")
 	print("completed find_and_kill")
 
@@ -38,7 +39,7 @@ func find_nearest_bird() -> Node2D:
 	var shortest_bird: Node2D
 	for b in get_tree().get_nodes_in_group("bird"):
 		yield(get_tree(), "idle_frame")
-		var p = ($"%BirdNav" as BirdNav).find_path(global_position, b.global_position)
+		var p = birdnav.find_path(global_position, b.global_position)
 		if !p: continue
 		if shortest:
 			if p.size() < shortest.size():
@@ -48,6 +49,8 @@ func find_nearest_bird() -> Node2D:
 			shortest = p
 			shortest_bird = b
 	yield(get_tree(), "idle_frame")
+	if !is_instance_valid(shortest_bird):
+		shortest_bird = null
 	return shortest_bird
 
 func _on_InteractionArea_area_entered(area: Area2D) -> void:
